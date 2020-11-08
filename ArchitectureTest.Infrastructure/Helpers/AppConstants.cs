81c757514362c6c96cf3a02cab9f271390ed2154
Miguel Angel López Arcos <migue300995@gmail.com>
2020-11-07 23:34:38 -0600

    Change App Constants

--- ArchitectureTest.Infrastructure/Helpers/AppConstants.cs
+++ ArchitectureTest.Infrastructure/Helpers/AppConstants.cs
@@ -1,11 +1,10 @@
 ﻿namespace ArchitectureTest.Infrastructure.Helpers {
 	public class AppConstants {
 		public const string SessionCookie = "__session";
-		public const string UserId = "usrId";
-		public const string Email = "email";
-		public const string Name = "name";
 
 		public const string Token = "tkn";
 		public const string RefreshToken = "rfshTkn";
+		public const string RefreshTokenHeader = "Refresh_Token";
+		public const string SaveAuthInCookieHeader = "Auth_In_Cookie";
 	}
 }

8b44735a475a402028345da4e24ed4bb0b99535b
Miguel Angel Lopez Arcos <migue300995@gmail.com>
2020-11-14 00:10:20 -0600

    Update database script

--- Database.sql
+++ Database.sql
@@ -3,13 +3,6 @@ CREATE TABLE User (Id BIGINT PRIMARY KEY AUTO_INCREMENT, Email VARCHAR(320), Use
 CREATE TABLE Note (Id BIGINT PRIMARY KEY AUTO_INCREMENT, UserId BIGINT NOT NULL, Title VARCHAR(100), Content TEXT, CreationDate DATETIME DEFAULT NOW(), ModificationDate DATETIME DEFAULT NOW(), FOREIGN KEY (UserId) REFERENCES User(Id));
 CREATE TABLE Checklist (Id BIGINT PRIMARY KEY AUTO_INCREMENT, UserId BIGINT NOT NULL, Title VARCHAR(100), CreationDate DATETIME DEFAULT NOW(), ModificationDate DATETIME DEFAULT NOW(), FOREIGN KEY (UserId) REFERENCES User(Id));
 CREATE TABLE ChecklistDetail (Id BIGINT PRIMARY KEY AUTO_INCREMENT, ChecklistId BIGINT NOT NULL, ParentDetailId BIGINT, TaskName VARCHAR(100) NOT NULL, Status BIT NOT NULL, CreationDate DATETIME DEFAULT NOW(), ModificationDate DATETIME DEFAULT NOW(), FOREIGN KEY (ChecklistId) REFERENCES Checklist(Id));
-
-
-
-INSERT INTO User (Email, UserName) VALUES ('migue300995@gmail.com', 'Miguel Angel');
-
-INSERT INTO Note (UserId, Title, Content) VALUES (1, 'Note 1', ''), (1, 'Note 2', ''), (1, 'Note 3', ''), (1, 'Note 4', ''), (1, 'Note 5', ''), (1, 'Note 16', ''), (1, 'Note 7', '');
-
 -- dotnet ef dbcontext scaffold "Server=localhost; Database=Papu; Uid=root; Pwd=root" Pomelo.EntityFrameworkCore.MySql -c DatabaseContext --context-dir Database/MySQL -o Database/MySQL/Entities --force
 
 
@@ -19,11 +12,23 @@ CREATE TABLE Note (Id BIGINT PRIMARY KEY IDENTITY(1, 1), UserId BIGINT NOT NULL,
 CREATE TABLE Checklist (Id BIGINT PRIMARY KEY IDENTITY(1, 1), UserId BIGINT NOT NULL, Title VARCHAR(100), CreationDate DATETIME DEFAULT GETDATE(), ModificationDate DATETIME DEFAULT GETDATE(), FOREIGN KEY (UserId) REFERENCES [User](Id));
 CREATE TABLE ChecklistDetail (Id BIGINT PRIMARY KEY IDENTITY(1, 1), ChecklistId BIGINT NOT NULL, ParentDetailId BIGINT, TaskName VARCHAR(100) NOT NULL, Status BIT NOT NULL, CreationDate DATETIME DEFAULT GETDATE(), ModificationDate DATETIME DEFAULT GETDATE(), FOREIGN KEY (ChecklistId) REFERENCES Checklist(Id));
 
+-- dotnet ef dbcontext scaffold "Server=(localdb)\mssqllocaldb;Database=ArchitectureTest;Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer -c DatabaseContext --context-dir Database/SQLServer -o Database/SQLServer/Entities --force
 
-
+---- INSERTS
 INSERT INTO [User] (Email, UserName) VALUES ('migue300995@gmail.com', 'Miguel Angel');
 
 INSERT INTO Note (UserId, Title, Content) VALUES (1, 'Note 1', ''), (1, 'Note 2', ''), (1, 'Note 3', ''), (1, 'Note 4', ''), (1, 'Note 5', ''), (1, 'Note 16', ''), (1, 'Note 7', '');
+INSERT INTO Checklist(UserId, Title) VALUES (1, 'Checklist 1')
+INSERT INTO ChecklistDetail(ChecklistId, ParentDetailId, TaskName, Status) VALUES (1, NULL, 'Limpiar cuarto', 0), (1, NULL, 'Limpiar oficina', 0), (1, NULL, 'Bañar perro', 0), (1, NULL, 'Hacer tarea', 0)
+INSERT INTO Checklist(UserId, Title) VALUES (1, 'Animales')
+INSERT INTO ChecklistDetail(ChecklistId, ParentDetailId, TaskName, Status) VALUES (2, NULL, 'Vertebrados', 0), (2, NULL, 'Invertebrados', 0)
+INSERT INTO ChecklistDetail(ChecklistId, ParentDetailId, TaskName, Status) VALUES (1, 5, 'Mamiferos', 0), (1, 5, 'Peces', 0), (1, 5, 'Aves', 0), (1, 5, 'Reptiles', 0), (1, 5, 'Anfibios', 0)
+INSERT INTO ChecklistDetail(ChecklistId, ParentDetailId, TaskName, Status) VALUES (1, 7, 'Leon', 0), (1, 7, 'Perro', 0), (1, 7, 'Ballena', 0)
+INSERT INTO ChecklistDetail(ChecklistId, ParentDetailId, TaskName, Status) VALUES (1, 8, 'Pez Ballena', 0)
+INSERT INTO ChecklistDetail(ChecklistId, ParentDetailId, TaskName, Status) VALUES (1, 9, 'Aguila', 0), (1, 9, 'Cito', 0)
+INSERT INTO ChecklistDetail(ChecklistId, ParentDetailId, TaskName, Status) VALUES (1, 10, 'Serpiente', 0), (1, 10, 'Cocodrilo', 0), (1, 10, 'Tortuga', 0)
+INSERT INTO ChecklistDetail(ChecklistId, ParentDetailId, TaskName, Status) VALUES (1, 11, 'Rana', 0)
+
+INSERT INTO ChecklistDetail(ChecklistId, ParentDetailId, TaskName, Status) VALUES (1, 6, 'Gusanos', 0), (1, 6, 'Esponjas', 0), (1, 6, 'Estrellas de mar', 0)
 
 
-dotnet ef dbcontext scaffold "Server=(localdb)\mssqllocaldb;Database=ArchitectureTest;Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer -c DatabaseContext --context-dir Database/SQLServer -o Database/SQLServer/Entities --force

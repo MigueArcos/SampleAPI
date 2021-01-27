c60339e0c3662d7f3ba991e57f8c56c689b8f713
Miguel Angel López Arcos <migue300995@gmail.com>
2021-01-27 11:24:45 -0600

    Add requiered annotations to sign-in/up models

--- ArchitectureTest.Domain/Models/SignIn.cs
+++ ArchitectureTest.Domain/Models/SignIn.cs
@@ -3,9 +3,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ArchitectureTest.Domain.Models {
 	public class SignInModel {
-		[EmailAddress(ErrorMessage = CustomMessages.InvalidEmail)]
+        [Required(ErrorMessage = CustomMessages.InvalidEmail)]
+        [EmailAddress(ErrorMessage = CustomMessages.InvalidEmail)]
 		public string Email { get; set; }
-		[MinLength(6, ErrorMessage = CustomMessages.InvalidPassword)]
+        [Required(ErrorMessage = CustomMessages.InvalidEmail)]
+        [MinLength(6, ErrorMessage = CustomMessages.InvalidPassword)]
 		public string Password { get; set; }
 	}
 }
--- ArchitectureTest.Domain/Models/SignUp.cs
+++ ArchitectureTest.Domain/Models/SignUp.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ArchitectureTest.Domain.Models {
 	public class SignUpModel : SignInModel {
+        [Required(ErrorMessage = CustomMessages.InvalidUserName)]
 		[MinLength(4, ErrorMessage = CustomMessages.InvalidUserName)]
 		public string UserName { get; set; }
 	}
